a4ca944d4378d9c9a366713c421c2351e4d2b65e
gulraizramzan28 <151021910+gulraizramzan28@users.noreply.github.com>
2023-11-16 15:59:16 +0500

    Update cloudinaryRoute.js

--- routers/cloudinaryRoute.js
+++ routers/cloudinaryRoute.js
@@ -1,15 +1,15 @@
-import multer from "multer";
-import express from "express";
-const upload = multer({ dest: "uploads/" });
+// import multer from "multer";
+// import express from "express";
+// const upload = multer({ dest: "uploads/" });
 
-const router = express.Router();
+// const router = express.Router();
 
-router.post("/upload", upload.single("image"), (req, res) => {
-  const imagePath = req.file.path;
-  cloudinary.uploader.upload(imagePath, (result) => {
-    const imageUrl = result.secure_url;
-    res.send({ imageUrl });
-  });
-});
+// router.post("/upload", upload.single("image"), (req, res) => {
+//   const imagePath = req.file.path;
+//   cloudinary.uploader.upload(imagePath, (result) => {
+//     const imageUrl = result.secure_url;
+//     res.send({ imageUrl });
+//   });
+// });
 
-export default router;
+// export default router;
